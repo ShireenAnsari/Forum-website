f26a562d238b23bfa16a2255265f9588c6174862
shiri <30csstudent@gmail.com>
2023-04-14 22:21:47 +0500

    removed fixed top as it was causing some Problem might set latter

--- Partials/_header.php
+++ Partials/_header.php
@@ -1,6 +1,6 @@
 
 <?php
-echo'<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+echo'<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 <div class="container-fluid">
   <a class="navbar-brand" href="Welcome.php">QNAHUB</a>
   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
